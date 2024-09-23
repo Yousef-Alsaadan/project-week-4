7e539c7f03836981f9619a022ec64f3344a8f9da
fahd alghtni <fahdalghtni246@gmail.com>
2024-09-23 13:35:53 +0300

    update 4 fahd

---
+++ info.js
@@ -0,0 +1,20 @@
+if(window.innerWidth<=600){
+document.querySelectorAll('.info-box').forEach(box=>{
+    const p =box.querySelector('p');
+    const text =p.innerText;
+
+    const numtext= text.slice(0,100);
+    if(text.length>100){
+        p.innerText=numtext + '...';
+         const showbutton=document.createElement('span');
+         showbutton.classList.add('show-more');
+         showbutton.innerText='عرض المزيد';
+
+         showbutton.addEventListener('click',function(){
+            p.innerText=text;
+            showbutton.style.display='none';
+         });
+         box.appendChild(showbutton);
+    }
+})
+}
